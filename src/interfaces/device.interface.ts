a4b531829dfcd06778cf1f0148bf97119c8abd43
Der Alex <aw@7pkonzepte.de>
2020-10-29 12:00:02 +0100

    added simple sizes

--- src/interfaces/device.interface.ts
+++ src/interfaces/device.interface.ts
@@ -1,6 +1,18 @@
 import { deviceSizes, deviceOrientations } from './device.enum';
 
+export interface SimpleDeviceSizeInterface {
+  xxs: number;
+  xs: number;
+  s: number;
+  m: number;
+  l: number;
+  xl: number;
+  xxl: number;
+}
+
 export interface DeviceInterface {
+  simpleDeviceSize: string;
   deviceSize: deviceSizes;
   deviceOrientation: deviceOrientations;
 }
+
--- src/store/modules/device.ts
+++ src/store/modules/device.ts
@@ -1,17 +1,31 @@
 import { Module } from 'vuex';
 import { deviceSizes, deviceOrientations } from '../../interfaces/device.enum';
-import { DeviceInterface } from '../../interfaces/device.interface';
+import {DeviceInterface, SimpleDeviceSizeInterface} from '../../interfaces/device.interface';
 import { RootStateInterface } from '../../interfaces/rootState.interface';
+
+const sizes: SimpleDeviceSizeInterface = {
+  xxs: 480,
+  xs: 560,
+  s: 640,
+  m: 768,
+  l: 960,
+  xl: 1024,
+  xxl: 1216
+};
 
 const deviceModule: Module<DeviceInterface, RootStateInterface> = {
   namespaced: true,
   state: {
+    simpleDeviceSize: Object.keys(sizes)[0],
     deviceSize: deviceSizes.mobile,
     deviceOrientation: deviceOrientations.portrait
   },
   getters: {
     getDeviceOrientation(state: DeviceInterface) {
       return state.deviceOrientation;
+    },
+    getSimpleDeviceSize(state: DeviceInterface) {
+      return state.simpleDeviceSize;
     },
     getDeviceSize(state: DeviceInterface) {
       return deviceSizes[state.deviceSize];
@@ -29,20 +43,24 @@ const deviceModule: Module<DeviceInterface, RootStateInterface> = {
   mutations: {
     SET_DEVICE_SIZE(state: DeviceInterface, deviceSize: deviceSizes) {
       state.deviceSize = deviceSize;
+    },
+    SET_SIMPLE_DEVICE_SIZE(state: DeviceInterface, simpleSize: string) {
+      state.simpleDeviceSize = simpleSize;
     }
   },
   actions: {
     setDeviceSize({ state, commit }, deviceWidth: number) {
-      const sizes = {
-        xxs: 480,
-        xs: 560,
-        s: 640,
-        m: 768,
-        l: 960,
-        xl: 1024,
-        xxl: 1216
-      };
-
+      let simpleSize = state.simpleDeviceSize;
+      const sizesKeys: (keyof SimpleDeviceSizeInterface)[] = Object.keys(sizes) as ((keyof SimpleDeviceSizeInterface)[]);
+      
+      sizesKeys.forEach(size => {
+        if (deviceWidth <= sizes[size] ) {
+          simpleSize = size.toString();
+        }
+      });
+  
+      commit('SET_SIMPLE_DEVICE_SIZE', simpleSize);
+  
       if (deviceWidth < sizes.s) {
         if (state.deviceSize !== deviceSizes.mobile) {
           commit('SET_DEVICE_SIZE', deviceSizes.mobile);
